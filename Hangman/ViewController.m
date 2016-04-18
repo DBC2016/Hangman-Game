6f3543954b8c7fc1656cd179c5ded982e8bf7656
Demond Childers <Demond@demonds-mbp.attlocal.net>
2016-04-18 01:55:32 -0400

    Hangman game... in progress.

--- Hangman/ViewController.m
+++ Hangman/ViewController.m
@@ -10,18 +10,97 @@
 
 @interface ViewController ()
 
+#pragma mark - Properties
+
+//@property (nonatomic, weak)     IBOutlet UIImageView    *backgroundImageview;
+//@property (nonatomic, strong)            NSArray        *hangmanImageArray;
+@property (nonatomic, strong)            NSArray        *wordListArray;
+@property (nonatomic, strong)            NSString       *currentWord;
+@property (nonatomic, strong)            NSString       *correctLetter;
+@property (nonatomic, strong)            NSString       *wrongLetter;
+
 @end
 
 @implementation ViewController
 
+#pragma mark - Global Variables
+
+int failedAttemptCount = 0;
+
+#pragma mark - Core Methods
+
+- (NSString *)readBundleFileToString:(NSString *)filename ofType:(NSString *)type {
+    NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:type];
+    return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+}
+
+- (NSArray *)convertCSVStringToArray:(NSString *)csvString {
+    NSString *cleanString = [[csvString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@""];
+    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@","];
+    return [cleanString componentsSeparatedByCharactersInSet:set];
+}
+
+- (void)randomWords {
+    long randomWordIndex = arc4random_uniform((uint32_t)_wordListArray.count);
+    _currentWord = [_wordListArray[randomWordIndex] lowercaseString];
+    NSLog(@"%@",_currentWord);
+}
+
+#pragma mark - Interactivity Methods
+
+- (IBAction)newGameButtonPressed:(id)sender {
+    NSLog(@"New Game");
+    failedAttemptCount = 0;
+    [self randomWords];
+}
+
+- (IBAction)letterPressed: (UIButton *)button {
+    NSString *buttonLetter = [button.titleLabel.text lowercaseString];
+    bool isFound = false;
+    for (int i = 0; i < [_currentWord length]; i++) {
+        NSString *currentLetter = [_currentWord substringWithRange:NSMakeRange(i, 1)];
+        if ([currentLetter isEqualToString:buttonLetter]) {
+            NSLog(@"Found an %@ at %i", buttonLetter,i);
+            isFound = true;
+        } else {
+            NSLog(@"Didn't find an %@ at %i", buttonLetter,i);
+        }
+    }
+//Number of Tries
+    if (!isFound) {
+        failedAttemptCount++;
+        NSLog(@"You Now Have %i Wrong",failedAttemptCount);
+    } else {
+        NSLog(@"Good Guess");
+    }
+    if (failedAttemptCount >= 10) {
+        NSLog(@"Game Over");
+    }
+}
+
+//if (_wordLabel.text == _currentWord) {
+//    gameover = true;
+//    [self disableAllLetters];
+//    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Congratulations" message:@"You have won!" preferredStyle:UIAlertControllerStyleAlert];
+//    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+//    [alert addAction:defaultAction];
+//    UIAlertAction* newAction = [UIAlertAction actionWithTitle:@"New Game" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+//        [self startGame];
+//    }];
+//    [alert addAction:newAction];
+//    [self presentViewController:alert animated:YES completion:nil];
+//
+#pragma mark - Life Cycle Methods
+
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *wordListString = [self readBundleFileToString:@"WordSetApple" ofType:@"csv"];
+    _wordListArray = [self convertCSVStringToArray:wordListString];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
